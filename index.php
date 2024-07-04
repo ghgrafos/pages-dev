d070da024ff26bb1816b8039271f567a20beeb6a
gilmar <grafos@outlook.com.br>
2024-07-04 12:13:21 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+if (PHP_SAPI === 'cli-server') {
+    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+
+    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $file = __DIR__ . $url['path'];
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+        return false;
+    }
+}
+
+include('index.html');
+
+?>
